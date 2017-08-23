bf0dbca108695cbdf3454874d1c0bcb62f201eba
Arnov Sharma <31111473+razorArnov@users.noreply.github.com>
2017-08-24 01:29:24 +0530

    Add files via upload

---
+++ ldr_bulb.ino
@@ -0,0 +1,39 @@
+int sensorPin = A0; // select the input pin for the LDR
+
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+int relay = 2;
+
+void setup() { // declare the ledPin as an OUTPUT:
+
+pinMode(relay, OUTPUT);
+
+Serial.begin(9600); }
+
+void loop()
+
+{
+
+Serial.println("light is on");
+
+sensorValue = analogRead(sensorPin);
+
+Serial.println(sensorValue);
+
+if (sensorValue < 100)
+
+{
+
+Serial.println("relay on");
+
+digitalWrite(relay,HIGH);
+
+delay(1000);
+
+}
+
+digitalWrite(relay,LOW);
+
+delay(sensorValue);
+
+}
